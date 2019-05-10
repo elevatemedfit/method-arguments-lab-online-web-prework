b47e599cc8c98eaea2aa66f949e3ed471da4ede6
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-10 14:36:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@ def introduction_with_language(name,language)
 end
 
 introduction_with_language("Dan", "Ember.js")
+
+def introduction(name)
+  puts "Hi, my name is ${name}."
+end
+
+introduction("Josh")
